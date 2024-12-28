ff64f91c59efc147bd8e1cd9f53266f1880f88a9
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2024-12-28 17:15:50 +0200

    create Routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { Navigate, Route, Routes } from 'react-router'
+import Layout from './layouts/Layout.jsx'
+import Register from './pages/Register/Register.jsx'
+import Login from './pages/Login/Login.jsx'
+import Welcome from './pages/Welcome/Welcome.jsx'
+import Recommended from './pages/Recommended/Recommended.jsx'
+import Library from './pages/Library/Library.jsx'
+import Reading from './pages/Reading/Reading.jsx'
+
 function App() {
   return (
-    <>
-      <div>app</div>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Welcome />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="recommended" element={<Recommended />} />
+        <Route path="library" element={<Library />} />
+        <Route path="reading" element={<Reading />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
+    </Routes>
   )
 }
 
