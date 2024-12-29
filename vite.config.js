781831d53a615ec25b64c38b80b493c6944e3272
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2024-12-29 15:52:15 +0200

    add alias

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@/assets': '/src/assets',
+      '@/components': '/src/components',
+      '@/layouts': '/src/layouts',
+      '@/pages': '/src/pages',
+      '@/services': '/src/services',
+      '@/store': '/src/store',
+      '@/utils': '/src/utils'
+    }
+  }
 })
