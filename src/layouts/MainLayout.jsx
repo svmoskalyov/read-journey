1bebd7131416bbe2a270957d43448da29ab24cdb
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2024-12-30 10:56:08 +0200

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,18 @@
 import { Suspense } from 'react'
 import { Outlet } from 'react-router'
+import { Grid, GridItem } from '@chakra-ui/react'
+import Header from '@/components/Header.jsx'
 
 function MainLayout() {
   return (
-    <Suspense fallback={null}>
-      <Outlet />
-    </Suspense>
+    <Grid grid="'header' 57px 'main' 1fr / 1fr" gap="2">
+      <Header />
+      <GridItem gridArea="main">
+        <Suspense fallback={null}>
+          <Outlet />
+        </Suspense>
+      </GridItem>
+    </Grid>
   )
 }
 
