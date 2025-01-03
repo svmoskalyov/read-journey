ec154b7d11b90ce0fe3983baaebf8cefe43d9922
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-01-03 09:24:59 +0200

    Update UserNav.jsx

--- src/components/UserNav.jsx
+++ src/components/UserNav.jsx
@@ -1,21 +1,23 @@
 import { Link, Tabs } from '@chakra-ui/react'
 
 function UserNav() {
+  const local = window.location.pathname
+
   return (
-    <Tabs.Root defaultValue="home">
+    <Tabs.Root defaultValue={local}>
       <Tabs.List
         display="flex"
         flexDirection="column"
         colorPalette="blue"
         borderBottom="none"
       >
-        <Tabs.Trigger value="home" asChild>
-          <Link unstyled href="#" color="brand.accent" p="0">
+        <Tabs.Trigger value="/recommended" asChild>
+          <Link unstyled href="/" color="brand.accent" p="0">
             Home
           </Link>
         </Tabs.Trigger>
-        <Tabs.Trigger value="library" asChild>
-          <Link unstyled href="#" color="brand.accent" p="0">
+        <Tabs.Trigger value="/library" asChild>
+          <Link unstyled href="library" color="brand.accent" p="0">
             My library
           </Link>
         </Tabs.Trigger>
