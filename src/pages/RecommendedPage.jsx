e93f1ade3c520087c67544fe292bf728db392374
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-01-03 09:25:07 +0200

    Update RecommendedPage.jsx

--- src/pages/RecommendedPage.jsx
+++ src/pages/RecommendedPage.jsx
@@ -1,21 +1,15 @@
 import { Flex, Grid } from '@chakra-ui/react'
 import Dashboard from '@/components/Dashboard'
-import RecommendedBooks from '@/components/RecommendedBooks'
+import Recommended from '@/components/Recommended'
 
 function RecommendedPage() {
   return (
     <Grid gap="2.5" bg="brand.bgPrimary" color="brand.accent">
-      <Flex
-        direction="column"
-        p="5"
-        h="478px"
-        bg="brand.bgSecondary"
-        rounded="30px"
-      >
-        <Dashboard />
+      <Flex direction="column" p="5" bg="brand.bgSecondary" rounded="30px">
+        <Dashboard page="recommended" />
       </Flex>
       <Flex px="5" py="10" h="382px" bg="brand.bgSecondary" rounded="30px">
-        <RecommendedBooks />
+        <Recommended />
       </Flex>
     </Grid>
   )
