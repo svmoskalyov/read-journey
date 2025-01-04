a55764192982dd0e298557db9d6998135de8145c
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-01-04 14:35:36 +0200

    Update MyLibraryPage.jsx

--- src/pages/MyLibraryPage.jsx
+++ src/pages/MyLibraryPage.jsx
@@ -1,6 +1,17 @@
+import { Flex, Grid } from '@chakra-ui/react'
+import Dashboard from '@/components/Dashboard'
+import MyLibrary from '@/components/MyLibrary'
+
 function MyLibraryPage() {
   return (
-    <div>MyLibraryPage</div>
+    <Grid gap="2.5" bg="brand.bgPrimary" color="brand.accent">
+      <Flex direction="column" p="5" bg="brand.bgSecondary" rounded="30px">
+        <Dashboard page="library" />
+      </Flex>
+      <Flex px="5" py="10" maxW='335px' h="382px" bg="brand.bgSecondary" rounded="30px">
+        <MyLibrary />
+      </Flex>
+    </Grid>
   )
 }
 
