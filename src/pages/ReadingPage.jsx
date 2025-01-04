b685902df563fbee40dc9a14eb7b882c3a7fa46e
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-01-04 14:35:19 +0200

    Update ReadingPage.jsx

--- src/pages/ReadingPage.jsx
+++ src/pages/ReadingPage.jsx
@@ -1,6 +1,33 @@
+import DashboardStat from '@/components/DashboardStat'
+import MyReading from '@/components/MyReading'
+import { Flex, Grid } from '@chakra-ui/react'
+
 function ReadingPage() {
   return (
-    <div>ReadingPage</div>
+    <Grid gap="2.5" bg="brand.bgPrimary" color="brand.accent">
+      <Flex
+        direction="column"
+        // justifyContent="space-between"
+        maxW="335px"
+        h="390px"
+        p="5"
+        bg="brand.bgSecondary"
+        rounded="30px"
+      >
+        <DashboardStat />
+      </Flex>
+      <Flex
+        p="5"
+        // px="5"
+        // py="10"
+        maxW="335px"
+        h="471px"
+        bg="brand.bgSecondary"
+        rounded="30px"
+      >
+        <MyReading />
+      </Flex>
+    </Grid>
   )
 }
 
