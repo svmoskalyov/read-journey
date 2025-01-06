ac5a9da4ef6d313be73567948bab36cb915aa852
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-01-06 10:42:46 +0200

    Update DialogBookStat.jsx

--- src/components/DialogBookStat.jsx
+++ src/components/DialogBookStat.jsx
@@ -1,14 +1,15 @@
+import { useState } from 'react'
+import { Heading, Image, Mark, Text } from '@chakra-ui/react'
 import {
   DialogBody,
   DialogCloseTrigger,
   DialogContent,
   DialogRoot
 } from '@/components/ui/dialog'
-import { Heading, Image, Mark, Text } from '@chakra-ui/react'
 import like from '@/assets/icons/like.svg'
-import { useState } from 'react'
+import books from '@/assets/icons/books.svg'
 
-function DialogBookStat({ onClose }) {
+function DialogBookStat({ statBook, onClose }) {
   const [open, setOpen] = useState(true)
 
   const toogle = e => {
@@ -32,7 +33,11 @@ function DialogBookStat({ onClose }) {
           alignItems="center"
           justifyContent="center"
         >
-          <Image mb="5" h="50px" w="50px" src={like} alt="image like" />
+          {statBook ? (
+            <Image mb="5" h="50px" w="50px" src={like} alt="image like" />
+          ) : (
+            <Image mb="5" h="50px" w="50px" src={books} alt="image like" />
+          )}
           <Heading
             mb="2.5"
             fontFamily="Gilroy-Bold"
@@ -40,22 +45,41 @@ function DialogBookStat({ onClose }) {
             lineHeight="18px"
             letterSpacing="0.02em"
           >
-            My library
+            {statBook ? 'Good job' : 'The book is read'}
           </Heading>
-          <Text
-            fontFamily="Gilroy-Medium"
-            fontSize="14px"
-            lineHeight="18px"
-            letterSpacing="0.02em"
-            color="brand.muted"
-            textAlign="center"
-          >
-            Your book is now in{' '}
-            <Mark px="1" color="brand.accent">
-              the library!
-            </Mark>{' '}
-            The joy knows no bounds and now you can start your training
-          </Text>
+
+          {statBook ? (
+            <Text
+              fontFamily="Gilroy-Medium"
+              fontSize="14px"
+              lineHeight="18px"
+              letterSpacing="0.02em"
+              color="brand.muted"
+              textAlign="center"
+            >
+              Your book is now in{' '}
+              <Mark px="1" color="brand.accent">
+                the library!
+              </Mark>{' '}
+              The joy knows no bounds and now you can start your training
+            </Text>
+          ) : (
+            <Text
+              fontFamily="Gilroy-Medium"
+              fontSize="14px"
+              lineHeight="18px"
+              letterSpacing="0.02em"
+              color="brand.muted"
+              textAlign="center"
+            >
+              It was an
+              <Mark px="1" color="brand.accent">
+                exciting journey
+              </Mark>
+              , where each page revealed new horizons, and the characters became
+              inseparable friends.
+            </Text>
+          )}
         </DialogBody>
         <DialogCloseTrigger />
       </DialogContent>
