c29384f4ab0177fcaa6ee46b6c2125df404c020c
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-01-10 17:13:06 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+import { lazy } from 'react'
 import { Navigate, Route, Routes } from 'react-router'
 import AuthLayout from '@/layouts/AuthLayout.jsx'
-import MainLayout from '@/layouts/MainLayout.jsx'
-import RegisterPage from '@/pages/RegisterPage.jsx'
-import LoginPage from '@/pages/LoginPage.jsx'
 import WelcomePage from '@/pages/WelcomePage.jsx'
-import RecommendedPage from '@/pages/RecommendedPage.jsx'
-import MyLibraryPage from '@/pages/MyLibraryPage.jsx'
-import ReadingPage from '@/pages/ReadingPage.jsx'
+// import MainLayout from '@/layouts/MainLayout.jsx'
+// import RegisterPage from '@/pages/RegisterPage.jsx'
+// import LoginPage from '@/pages/LoginPage.jsx'
+// import RecommendedPage from '@/pages/RecommendedPage.jsx'
+// import MyLibraryPage from '@/pages/MyLibraryPage.jsx'
+// import ReadingPage from '@/pages/ReadingPage.jsx'
+
+// import useAuthStore from '@/stores/useAuthStore'
+
+const MainLayout = lazy(() => import('./layouts/MainLayout'))
+const RegisterPage = lazy(() => import('./pages/RegisterPage'))
+const LoginPage = lazy(() => import('./pages/LoginPage'))
+const RecommendedPage = lazy(() => import('./pages/RecommendedPage'))
+const MyLibraryPage = lazy(() => import('./pages/MyLibraryPage'))
+const ReadingPage = lazy(() => import('./pages/ReadingPage'))
 
 function App() {
+  // const isAuth = useAuthStore(state => state.isAuth)
   // const isAuth = false
   const isAuth = true
 
