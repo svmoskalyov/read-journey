acc5c9b354048848c5c59d58fd8fe3f48b78d271
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-01-12 09:29:18 +0200

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,32 @@
+import { useEffect } from 'react'
+import { useAuthStore } from '@/stores/authStore'
+import toast from '@/utils/toast'
 import LoginForm from '@/components/LoginForm'
 
 function LoginPage() {
+  const error = useAuthStore(state => state.error)
+
+  const createErrorMessage = error => {
+    if (`${error}` === 'auth/too-many-requests') {
+      toast('error', 'Too many requests.')
+    } else if (`${error}` === 'auth/network-request-failed') {
+      toast('error', 'Problem with network')
+    } else if (`${error}` === 'auth/invalid-login-credentials') {
+      toast('error', 'Email or password is not correct')
+    } else if (`${error}` === 'auth/invalid-credential') {
+      toast('error', 'User not found')
+    } else {
+      console.log(error)
+    }
+    return error
+  }
+
+  useEffect(() => {
+    if (error) {
+      createErrorMessage(error)
+    }
+  }, [error])
+
   return <LoginForm />
 }
 
