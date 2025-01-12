738a7d03c3161644db6d1d782e263e32d7dcd44a
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-01-12 09:29:16 +0200

    Update RegisterPage.jsx

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,30 @@
+import { useEffect } from 'react'
+import { useAuthStore } from '@/stores/authStore'
+import toast from '@/utils/toast'
 import RegisterForm from '@/components/RegisterForm.jsx'
 
 function RegisterPage() {
+  const error = useAuthStore(state => state.error)
+
+  const createErrorMessage = error => {
+    if (`${error}` === 'auth/too-many-requests') {
+      toast('error', 'Too many requests.')
+    } else if (`${error}` === 'auth/network-request-failed') {
+      toast('error', 'Problem with network')
+    } else if (`${error}` === 'auth/email-already-in-use') {
+      toast('error', 'Email already exists')
+    } else {
+      console.log(error)
+    }
+    return error
+  }
+
+  useEffect(() => {
+    if (error) {
+      createErrorMessage(error)
+    }
+  }, [error])
+
   return <RegisterForm />
 }
 
