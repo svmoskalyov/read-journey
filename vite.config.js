9c17463db09254e90ab2a258ff877a97ad5cd1f5
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-01-13 20:17:13 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     alias: {
       '@/assets': '/src/assets',
       '@/components': '/src/components',
+      '@/hooks': '/src/hooks',
       '@/layouts': '/src/layouts',
       '@/pages': '/src/pages',
       '@/services': '/src/services',
