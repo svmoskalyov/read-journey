6ebe6bf9797c478f46aa8e674d6c754a4268656f
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-01-14 10:13:36 +0200

    Update useMediaQuery.js

--- src/hooks/useMediaQuery.js
+++ src/hooks/useMediaQuery.js
@@ -10,23 +10,3 @@ export default function useMediaQuery(query) {
     }
   }, () => mediaQuery.current.matches)
 }
-
-// import { useState, useEffect } from 'react'
-//
-// const useMediaQuery = (query) => {
-//   const [matches, setMatches] = useState(false)
-//
-//   useEffect(() => {
-//     const media = window.matchMedia(query)
-//     if (media.matches !== matches) {
-//       setMatches(media.matches)
-//     }
-//     const listener = () => setMatches(media.matches)
-//     window.addEventListener('resize', listener)
-//     return () => window.removeEventListener('resize', listener)
-//   }, [matches, query])
-//
-//   return matches
-// }
-//
-// export default useMediaQuery
