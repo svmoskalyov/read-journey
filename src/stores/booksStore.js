40aa2f18fe5a1569848001f2dced2641074a0a52
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-01-16 13:25:07 +0200

    Update booksStore.js

--- src/stores/booksStore.js
+++ src/stores/booksStore.js
@@ -1,22 +1,91 @@
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
-import { getRecommendedApi } from '@/services/api'
+import { addBookApi, getRecommendedApi } from '@/services/api'
 
 export const useRecommendedStore = create()(
   persist(
     set =>
       ({
         books: [],
+        isLoading: false,
+        error: null,
         getBooks: async () => {
+          set({ isLoading: true })
           try {
             const data = await getRecommendedApi()
             set({ books: data })
           } catch (error) {
-            console.error(error)
+            set({ error: error.code })
+          } finally {
+            set({ isLoading: false })
           }
+        },
+        setBooks: () => {
+          set({ books: [] })
         }
       }),
     {
       name: 'books-recommended'
     }
   ))
+
+export const useLibraryStore = create()(
+  persist(
+    set =>
+      ({
+        books: [],
+        isAdded: false,
+        isRead: false,
+        isLoading: false,
+        error: null,
+        addBook: (book) => {
+          set({ isLoading: true })
+          try {
+            const newBook = {
+              ...book,
+              status: 'unread'
+            }
+            addBookApi(newBook)
+            set({ isAdded: true })
+          } catch (error) {
+            set({ error: error.code })
+          } finally {
+            set({ isLoading: false })
+          }
+        },
+        getBooks: async () => {
+          set({ isLoading: true })
+          try {
+          } catch (error) {
+            set({ error: error.code })
+          } finally {
+            set({ isLoading: false })
+          }
+        },
+        updateBook: async ({ id }) => {
+          set({ isLoading: true })
+          try {
+          } catch (error) {
+            set({ error: error.code })
+          } finally {
+            set({ isLoading: false })
+          }
+        },
+        setIsAdded: value => set({ isAdded: value }),
+        setIsRead: value => set({ isRead: value })
+      }),
+    {
+      name: 'books-library',
+      partialize: state =>
+        Object.fromEntries(
+          Object.entries(state).filter(
+            ([key]) =>
+              !['isLoading'].includes(key) &&
+              !['error'].includes(key) &&
+              !['isAdded'].includes(key) &&
+              !['isRead'].includes(key)
+          )
+        )
+    }
+  )
+)
