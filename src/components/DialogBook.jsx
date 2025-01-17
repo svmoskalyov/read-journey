ef61edaa9f41c8d93f401fc51cdaa1a6d9fdd4c7
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-01-17 17:19:01 +0200

    Update DialogBook.jsx

--- src/components/DialogBook.jsx
+++ src/components/DialogBook.jsx
@@ -1,142 +1,155 @@
 import { useState } from 'react'
 import { Card, Flex, Heading, Text } from '@chakra-ui/react'
 import { Button } from '@/components/ui/button'
-import { DialogBody, DialogCloseTrigger, DialogContent, DialogRoot
+import {
+  DialogBody, DialogCloseTrigger, DialogContent, DialogRoot
 } from '@/components/ui/dialog'
+import { useLibraryStore, useRecommendedStore } from '@/stores/booksStore.js'
 
-function DialogBook({ statBook, onClose, book }) {
+function DialogBook({ statBook, book }) {
   const [open, setOpen] = useState(true)
+  const addBook = useLibraryStore(state => state.addBook)
+  const changeStatus = useRecommendedStore(state => state.changeStatus)
 
   const toogle = e => {
     setOpen(e.open)
-    onClose()
+  }
+
+  const handleClick = () => {
+    if (statBook) {
+      addBook(book)
+      changeStatus(book.id)
+    }
   }
 
   return (
-    <DialogRoot lazyMount placement="center" open={open} onOpenChange={toogle}>
-      <DialogContent
-        p={{ base: '40px', tablet: '50px' }}
-        h={{ base: '421px', tablet: '483px' }}
-        w={{ base: '335px', tablet: '500px' }}
-        bg="brand.bgSecondary"
-        color="brand.accent"
-        rounded="12px"
-      >
-        <DialogBody display="flex" justifyContent="center">
-          <Card.Root
-            alignItems="center"
-            maxW="248px"
-            bg="brand.bgSecondary"
-            color="brand.accent"
-            border="none"
-            overflow="hidden"
-          >
-            <Flex
-              direction="column"
-              justifyContent="space-between"
+    <>
+      <DialogRoot lazyMount placement="center" open={open} onOpenChange={toogle}>
+        <DialogContent
+          p={{ base: '40px', tablet: '50px' }}
+          h={{ base: '421px', tablet: '483px' }}
+          w={{ base: '335px', tablet: '500px' }}
+          bg="brand.bgSecondary"
+          color="brand.accent"
+          rounded="12px"
+        >
+          <DialogBody display="flex" justifyContent="center">
+            <Card.Root
               alignItems="center"
-              mb="16px"
-              p="4"
-              h={{ base: '208px', tablet: '233px' }}
-              w={{ base: '137px', tablet: '153px' }}
-              rounded="8px"
-              bg={book.color}
-              boxShadow="0px 0px 16px 2px rgba(255,255,255,0.4) inset"
-              cursor="pointer"
+              maxW="248px"
+              bg="brand.bgSecondary"
+              color="brand.accent"
+              border="none"
+              overflow="hidden"
             >
-              <Text
-                maxW="98%"
-                fontFamily="Gilroy-Medium"
-                fontSize={{ base: '12px', tablet: '14px' }}
-                lineHeight={{ base: '14px', tablet: '18px' }}
-                letterSpacing="-0.02em"
-                overflow="hidden"
-                textWrap="nowrap"
-                textOverflow="ellipsis"
-                userSelect="none"
+              <Flex
+                direction="column"
+                justifyContent="space-between"
+                alignItems="center"
+                mb="16px"
+                p="4"
+                h={{ base: '208px', tablet: '233px' }}
+                w={{ base: '137px', tablet: '153px' }}
+                rounded="8px"
+                bg={book.color}
+                boxShadow="0px 0px 16px 2px rgba(255,255,255,0.4) inset"
+                cursor="pointer"
               >
-                {book.author}
-              </Text>
-              <Heading
-                maxH="50%"
-                fontFamily="Gilroy-Bold"
-                fontSize={{ base: '18px', tablet: '20px' }}
-                lineHeight={{ base: '20px', tablet: '22px' }}
-                letterSpacing="-0.02em"
-                overflow="hidden"
-                textOverflow="ellipsis"
-                textAlign="center"
-                userSelect="none"
-              >
-                {book.title}
-              </Heading>
-              <Text
-                maxW="98%"
-                fontFamily="Gilroy-Medium"
-                fontSize={{ base: '12px', tablet: '10px' }}
-                lineHeight={{ base: '14px', tablet: '12px' }}
-                letterSpacing="0.2em"
-                overflow="hidden"
-                textWrap="nowrap"
-                textOverflow="ellipsis"
-                userSelect="none"
-              >
-                {book.totalPages}
-              </Text>
-            </Flex>
+                <Text
+                  maxW="98%"
+                  fontFamily="Gilroy-Medium"
+                  fontSize={{ base: '12px', tablet: '14px' }}
+                  lineHeight={{ base: '14px', tablet: '18px' }}
+                  letterSpacing="-0.02em"
+                  overflow="hidden"
+                  textWrap="nowrap"
+                  textOverflow="ellipsis"
+                  userSelect="none"
+                >
+                  {book.author}
+                </Text>
+                <Heading
+                  maxH="50%"
+                  fontFamily="Gilroy-Bold"
+                  fontSize={{ base: '18px', tablet: '20px' }}
+                  lineHeight={{ base: '20px', tablet: '22px' }}
+                  letterSpacing="-0.02em"
+                  overflow="hidden"
+                  textOverflow="ellipsis"
+                  textAlign="center"
+                  userSelect="none"
+                >
+                  {book.title}
+                </Heading>
+                <Text
+                  maxW="98%"
+                  fontFamily="Gilroy-Medium"
+                  fontSize={{ base: '12px', tablet: '10px' }}
+                  lineHeight={{ base: '14px', tablet: '12px' }}
+                  letterSpacing="0.2em"
+                  overflow="hidden"
+                  textWrap="nowrap"
+                  textOverflow="ellipsis"
+                  userSelect="none"
+                >
+                  {book.totalPages}
+                </Text>
+              </Flex>
 
-            <Card.Body alignItems="center" gap="1" p="0" pb="5">
-              <Card.Title
-                fontFamily="Gilroy-Bold"
-                fontSize={{ base: '18px', tablet: '20px' }}
-                lineHeight={{ base: '20px', tablet: '24px' }}
-                letterSpacing="0.02em"
-                overflow="hidden"
-                textWrap="nowrap"
-                textOverflow="ellipsis"
-              >
-                {book.title}
-              </Card.Title>
-              <Card.Description
-                fontFamily="Gilroy-Medium"
-                fontSize={{ base: '12px', tablet: '14px' }}
-                lineHeight={{ base: '14px', tablet: '18px' }}
-                letterSpacing="0.02em"
-                overflow="hidden"
-                textWrap="nowrap"
-                textOverflow="ellipsis"
-              >
-                {book.author}
-              </Card.Description>
-              <Text
-                fontFamily="Gilroy-Medium"
-                fontSize={{ base: '12px', tablet: '10px' }}
-                lineHeight={{ base: '14px', tablet: '12px' }}
-                letterSpacing="0.2em"
-              >
-                {book.totalPages}
-              </Text>
-            </Card.Body>
-            <Card.Footer p="0">
-              <Button
-                variant="outline"
-                h={{ base: '42px', tablet: '46px' }}
-                w={{ base: '141px', tablet: '162px' }}
-                fontFamily="Gilroy-Bold"
-                fontSize={{ base: '14px', tablet: '16px' }}
-                lineHeight={{ base: '14px', tablet: '18px' }}
-                color="brand.accent"
-                border="1px solid #f9f9f94d"
-                rounded="30px"
-              >
-                {statBook ? 'Add to library' : 'Start reading'}
-              </Button>
-            </Card.Footer>
-          </Card.Root>
-        </DialogBody>
-        <DialogCloseTrigger />
-      </DialogContent>
-    </DialogRoot>
+              <Card.Body alignItems="center" gap="1" p="0" pb="5">
+                <Card.Title
+                  fontFamily="Gilroy-Bold"
+                  fontSize={{ base: '18px', tablet: '20px' }}
+                  lineHeight={{ base: '20px', tablet: '24px' }}
+                  letterSpacing="0.02em"
+                  overflow="hidden"
+                  textWrap="nowrap"
+                  textOverflow="ellipsis"
+                >
+                  {book.title}
+                </Card.Title>
+                <Card.Description
+                  fontFamily="Gilroy-Medium"
+                  fontSize={{ base: '12px', tablet: '14px' }}
+                  lineHeight={{ base: '14px', tablet: '18px' }}
+                  letterSpacing="0.02em"
+                  overflow="hidden"
+                  textWrap="nowrap"
+                  textOverflow="ellipsis"
+                >
+                  {book.author}
+                </Card.Description>
+                <Text
+                  fontFamily="Gilroy-Medium"
+                  fontSize={{ base: '12px', tablet: '10px' }}
+                  lineHeight={{ base: '14px', tablet: '12px' }}
+                  letterSpacing="0.2em"
+                >
+                  {book.totalPages}
+                </Text>
+              </Card.Body>
+              <Card.Footer p="0">
+                <Button
+                  variant="outline"
+                  h={{ base: '42px', tablet: '46px' }}
+                  w={{ base: '141px', tablet: '162px' }}
+                  fontFamily="Gilroy-Bold"
+                  fontSize={{ base: '14px', tablet: '16px' }}
+                  lineHeight={{ base: '14px', tablet: '18px' }}
+                  color="brand.accent"
+                  border="1px solid #f9f9f94d"
+                  rounded="30px"
+                  onClick={handleClick}
+                >
+                  {statBook ? 'Add to library' : 'Start reading'}
+                </Button>
+              </Card.Footer>
+            </Card.Root>
+          </DialogBody>
+          <DialogCloseTrigger />
+        </DialogContent>
+      </DialogRoot>
+    </>
   )
 }
 
