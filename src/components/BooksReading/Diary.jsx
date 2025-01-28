4b5633b44d2ee5c2a548b5aeaa97291007b9d420
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-01-28 13:34:47 +0200

    Update Diary.jsx

--- src/components/BooksReading/Diary.jsx
+++ src/components/BooksReading/Diary.jsx
@@ -1,3 +1,4 @@
+import { nanoid } from 'nanoid'
 import { Box, Center, Flex, Heading, Image, List, Mark, Text } from '@chakra-ui/react'
 import trash2 from '@/assets/icons/trash-2.svg'
 import blockStat from '@/assets/icons/block.svg'
@@ -47,24 +48,26 @@ function Diary() {
       book.progress[arr.length - 1].startReading)
 
     for (let i = 0; i <= diffDays; i++) {
-      const day = plusDay(book.progress[0].startReading, i)
+      let pages = 0
+      const date = plusDay(book.progress[0].startReading, i)
       const booksFilter = book.progress.filter(el =>
-        dateToLocal(el.startReading) === day)
+        dateToLocal(el.startReading) === date)
       const booksFiltered = booksFilter.map(el => {
+        const pid = nanoid()
+        pages = Math.floor(el.finishPage - el.startPage)
         const procent = procentReading(el.startPage, el.finishPage)
         const time = timeReading(el.startReading, el.finishReading)
-        const pages = Math.floor(el.finishPage - el.startPage)
         const pageHour = pagesReadingHour(el.startPage, el.finishPage,
           el.startReading, el.finishReading)
-        return { procent, time, pages, pageHour }
+        return { pid, procent, time, pageHour }
       })
-      newBooks.push({ date: day, items: booksFiltered })
+      newBooks.push({ date, pages, items: booksFiltered })
     }
     return newBooks
   }
 
   useEffect(() => {
-    if (!book.progress) return
+    if (!book) return
     setProgress(progressReading(book.progress))
   }, [book])
 
@@ -85,18 +88,11 @@ function Diary() {
         overflowY="auto"
         scrollbar="hidden"
       >
-        {/* {items.map((item, index) => (
-            <List.Item key={index}>
-              {item}
-            </List.Item>
-          ))} */}
-
-        {progress.map((item, id) => {
+        {progress.map((book, id) => {
           return (
             <List.Item
               key={id}
               position="relative"
-              justifyContent="space-between"
               ml="2"
               pl="4"
               color="brand.muted"
@@ -121,273 +117,90 @@ function Diary() {
                 <Box h="8px" w="8px" bg="brand.bgPrimary" borderRadius="2px"></Box>
               </Center>
 
-              <Flex direction="column">
-                <Heading
-                  mb="3.5"
-                  fontFamily="Gilroy-Bold"
-                  fontSize="12px"
-                  lineHeight="16px"
-                  letterSpacing="0.02em"
-                >
-                  {item.date}
-                </Heading>
-
-                <Heading
-                  as="h3"
-                  mb="1"
-                  fontFamily="Gilroy-Medium"
-                  fontSize="14px"
-                  lineHeight="18px"
-                  letterSpacing="0.02em"
-                  color="brand.accent"
-                >
-                  23%
-                </Heading>
-                <Text
-                  mb="32px"
-                  fontFamily="Gilroy-Medium"
-                  fontSize="10px"
-                  lineHeight="12px"
-                  letterSpacing="0.02em"
-                >
-                  34
-                  <Mark ml="2px">minutes</Mark>
-                </Text>
-              </Flex>
-
-              <Flex align="center" gap="2">
-                <Flex direction="column" align="center" w="50px">
+              <Flex direction="column" w="full">
+                <Flex justifyContent="space-between" mb="28px" pr="22px">
+                  <Heading
+                    fontFamily="Gilroy-Medium"
+                    fontSize="16px"
+                    lineHeight="18px"
+                    letterSpacing="0.02em"
+                  >
+                    {book.date}
+                  </Heading>
                   <Heading
                     as="h3"
-                    mb="3.5"
-                    fontFamily="Gilroy-Medium"
-                    fontSize="12px"
-                    lineHeight="16px"
+                    fontFamily="Gilroy-Regular"
+                    fontSize="14px"
+                    lineHeight="18px"
+                    letterSpacing="-0.02em"
                   >
-                    15
+                    {book.pages}
                     <Mark ml="2px">pages</Mark>
                   </Heading>
-
-                  <Box mb="7px">
-                    <Image src={blockStat} />
-                  </Box>
-
-                  <Text
-                    w="100%"
-                    fontFamily="Gilroy-Medium"
-                    fontSize="10px"
-                    lineHeight="12px"
-                    textAlign="center"
-                  >
-                    12
-                    <Mark ml="2px" textWrap="wrap">pages per huor</Mark>
-                  </Text>
                 </Flex>
 
-                <Image
-                  h="3.5"
-                  w="3.5"
-                  src={trash2}
-                  alt="delete day"
-                  cursor="pointer"
-                  onClick={() => console.log('delete item')}
-                />
+                {book.items.map((reading) => {
+                  return (
+                    <Flex key={reading.pid} direction="column">
+                      <Flex justifyContent="space-between">
+                        <Heading
+                          as="h3"
+                          fontFamily="Gilroy-Regular"
+                          fontSize="20px"
+                          lineHeight="20px"
+                          letterSpacing="0.02em"
+                          color="brand.accent"
+                        >
+                          {reading.procent}%
+                        </Heading>
+                        <Flex align="center" gap="8px" h="30px">
+                          <Box>
+                            <Image
+                              h="25px"
+                              w="60px"
+                              src={blockStat}
+                            />
+                          </Box>
+                          <Image
+                            h="3.5"
+                            w="3.5"
+                            src={trash2}
+                            alt="delete day"
+                            cursor="pointer"
+                            onClick={() => console.log('delete item')}
+                          />
+                        </Flex>
+                      </Flex>
+
+                      <Flex justifyContent="space-between" mb="22px">
+                        <Text
+                          fontFamily="Gilroy-Medium"
+                          fontSize="12px"
+                          lineHeight="14px"
+                          letterSpacing="-0.02em"
+                        >
+                          {reading.time}
+                          <Mark ml="2px">minutes</Mark>
+                        </Text>
+                        <Text
+                          pt="4px"
+                          pr="22px"
+                          w="82px"
+                          fontFamily="Gilroy-Medium"
+                          fontSize="12px"
+                          lineHeight="14px"
+                          textAlign="center"
+                          letterSpacing="-0.02em"
+                        >
+                          {reading.pageHour}
+                          <Mark ml="2px" textWrap="wrap">pages per huor</Mark>
+                        </Text>
+                      </Flex>
+                    </Flex>
+                  )})}
               </Flex>
             </List.Item>
-          )
-        })}
-
-        {/*<List.Item*/}
-        {/*  position="relative"*/}
-        {/*  justifyContent="space-between"*/}
-        {/*  ml="2"*/}
-        {/*  pl="4"*/}
-        {/*  color="brand.muted"*/}
-        {/*  borderLeft="3px solid"*/}
-        {/*  borderColor="brand.bgSecondary"*/}
-        {/*  // _hover={{*/}
-        {/*  //   '&>div:first-child, & h2': {*/}
-        {/*  //     borderColor: 'brand.accent',*/}
-        {/*  //     color: 'brand.accent'*/}
-        {/*  //   }*/}
-        {/*  // }}*/}
-        {/*>*/}
-        {/*  <Center*/}
-        {/*    position="absolute"*/}
-        {/*    top="0"*/}
-        {/*    left="-9.5px"*/}
-        {/*    h="16px"*/}
-        {/*    w="16px"*/}
-        {/*    bg="brand.muted"*/}
-        {/*    borderRadius="4px"*/}
-        {/*  >*/}
-        {/*    <Box h="8px" w="8px" bg="brand.bgPrimary" borderRadius="2px"></Box>*/}
-        {/*  </Center>*/}
-
-        {/*  <Flex direction="column">*/}
-        {/*    <Heading*/}
-        {/*      mb="3.5"*/}
-        {/*      fontFamily="Gilroy-Bold"*/}
-        {/*      fontSize="12px"*/}
-        {/*      lineHeight="16px"*/}
-        {/*      letterSpacing="0.02em"*/}
-        {/*    >*/}
-        {/*      21.10.2023*/}
-        {/*    </Heading>*/}
-        {/*    <Heading*/}
-        {/*      as="h3"*/}
-        {/*      mb="1"*/}
-        {/*      fontFamily="Gilroy-Medium"*/}
-        {/*      fontSize="14px"*/}
-        {/*      lineHeight="18px"*/}
-        {/*      letterSpacing="0.02em"*/}
-        {/*      color="brand.accent"*/}
-        {/*    >*/}
-        {/*      7.6%*/}
-        {/*    </Heading>*/}
-        {/*    <Text*/}
-        {/*      mb="32px"*/}
-        {/*      fontFamily="Gilroy-Medium"*/}
-        {/*      fontSize="10px"*/}
-        {/*      lineHeight="12px"*/}
-        {/*      letterSpacing="0.02em"*/}
-        {/*    >*/}
-        {/*      29 minutes*/}
-        {/*    </Text>*/}
-        {/*  </Flex>*/}
-
-        {/*  <Flex align="center" gap="2">*/}
-        {/*    <Flex direction="column" align="flex-end">*/}
-        {/*      <Heading*/}
-        {/*        as="h3"*/}
-        {/*        mb="3.5"*/}
-        {/*        fontFamily="Gilroy-Medium"*/}
-        {/*        fontSize="12px"*/}
-        {/*        lineHeight="16px"*/}
-        {/*      >*/}
-        {/*        42 pages*/}
-        {/*      </Heading>*/}
-
-        {/*      <Box mb="7px" h="18px" w="43px">*/}
-        {/*        <Image src={blockStat} />*/}
-        {/*      </Box>*/}
-
-        {/*      <Text*/}
-        {/*        w="43px"*/}
-        {/*        fontFamily="Gilroy-Medium"*/}
-        {/*        fontSize="10px"*/}
-        {/*        lineHeight="12px"*/}
-        {/*      >*/}
-        {/*        45 pages per huor*/}
-        {/*      </Text>*/}
-        {/*    </Flex>*/}
-
-        {/*    <Image*/}
-        {/*      h="3.5"*/}
-        {/*      w="3.5"*/}
-        {/*      src={trash2}*/}
-        {/*      alt="delete day"*/}
-        {/*      cursor="pointer"*/}
-        {/*      onClick={() => console.log('delete item')}*/}
-        {/*    />*/}
-        {/*  </Flex>*/}
-        {/*</List.Item>*/}
-
-        {/*<List.Item*/}
-        {/*  position="relative"*/}
-        {/*  justifyContent="space-between"*/}
-        {/*  ml="2"*/}
-        {/*  pl="4"*/}
-        {/*  color="brand.muted"*/}
-        {/*  borderLeft="3px solid"*/}
-        {/*  borderColor="brand.bgSecondary"*/}
-        {/*  // _hover={{*/}
-        {/*  //   '&>div:first-child, & h2': {*/}
-        {/*  //     borderColor: 'brand.accent',*/}
-        {/*  //     color: 'brand.accent'*/}
-        {/*  //   }*/}
-        {/*  // }}*/}
-        {/*>*/}
-        {/*  <Center*/}
-        {/*    position="absolute"*/}
-        {/*    top="0"*/}
-        {/*    left="-9.5px"*/}
-        {/*    h="16px"*/}
-        {/*    w="16px"*/}
-        {/*    bg="brand.muted"*/}
-        {/*    borderRadius="4px"*/}
-        {/*  >*/}
-        {/*    <Box h="8px" w="8px" bg="brand.bgPrimary" borderRadius="2px"></Box>*/}
-        {/*  </Center>*/}
-
-        {/*  <Flex direction="column">*/}
-        {/*    <Heading*/}
-        {/*      mb="3.5"*/}
-        {/*      fontFamily="Gilroy-Bold"*/}
-        {/*      fontSize="12px"*/}
-        {/*      lineHeight="16px"*/}
-        {/*      letterSpacing="0.02em"*/}
-        {/*    >*/}
-        {/*      21.10.2023*/}
-        {/*    </Heading>*/}
-        {/*    <Heading*/}
-        {/*      as="h3"*/}
-        {/*      mb="1"*/}
-        {/*      fontFamily="Gilroy-Medium"*/}
-        {/*      fontSize="14px"*/}
-        {/*      lineHeight="18px"*/}
-        {/*      letterSpacing="0.02em"*/}
-        {/*      color="brand.accent"*/}
-        {/*    >*/}
-        {/*      7.6%*/}
-        {/*    </Heading>*/}
-        {/*    <Text*/}
-        {/*      mb="32px"*/}
-        {/*      fontFamily="Gilroy-Medium"*/}
-        {/*      fontSize="10px"*/}
-        {/*      lineHeight="12px"*/}
-        {/*      letterSpacing="0.02em"*/}
-        {/*    >*/}
-        {/*      29 minutes*/}
-        {/*    </Text>*/}
-        {/*  </Flex>*/}
-
-        {/*  <Flex align="center" gap="2">*/}
-        {/*    <Flex direction="column" align="flex-end">*/}
-        {/*      <Heading*/}
-        {/*        as="h3"*/}
-        {/*        mb="3.5"*/}
-        {/*        fontFamily="Gilroy-Medium"*/}
-        {/*        fontSize="12px"*/}
-        {/*        lineHeight="16px"*/}
-        {/*      >*/}
-        {/*        42 pages*/}
-        {/*      </Heading>*/}
-
-        {/*      <Box mb="7px" h="18px" w="43px">*/}
-        {/*        <Image src={blockStat} />*/}
-        {/*      </Box>*/}
-
-        {/*      <Text*/}
-        {/*        w="43px"*/}
-        {/*        fontFamily="Gilroy-Medium"*/}
-        {/*        fontSize="10px"*/}
-        {/*        lineHeight="12px"*/}
-        {/*      >*/}
-        {/*        45 pages per huor*/}
-        {/*      </Text>*/}
-        {/*    </Flex>*/}
-
-        {/*    <Image*/}
-        {/*      h="3.5"*/}
-        {/*      w="3.5"*/}
-        {/*      src={trash2}*/}
-        {/*      alt="delete day"*/}
-        {/*      cursor="pointer"*/}
-        {/*      onClick={() => console.log('delete item')}*/}
-        {/*    />*/}
-        {/*  </Flex>*/}
-        {/*</List.Item>*/}
+          )})}
       </List.Root>
     </Flex>
   )
